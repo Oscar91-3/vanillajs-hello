0fcbf5f50edd2946d91586accfd12b6651628e48
Oscar91-3 <rodrigezoscar139@gmail.com>
2024-08-28 02:26:10 +0000

    Generador excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomwho = Math.floor(Math.random() * who.length);
+  let randomaction = Math.floor(Math.random() * action.length);
+  let randomwhat = Math.floor(Math.random() * what.length);
+  let randomwhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[randomwho] +
+    " " +
+    action[randomaction] +
+    " " +
+    what[randomwhat] +
+    " " +
+    when[randomwhen]
+  );
 };
